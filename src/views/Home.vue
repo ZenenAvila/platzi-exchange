45cb1e24376d2456213def03c7840bcd87bdc20c
ZenenAvila <zenenavilasuarez@gmail.com>
2022-02-03 19:50:54 -0600

    Guardadando avances: router creado

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <px-assets-table />
+    <router-link to="/about" class="mt-5 text-xl text-green-600 hover:underline"
+      >Ir a about</router-link
+    >
+  </div>
+</template>
+
+<script>
+import PxAssetsTable from "@/components/PxAssetsTable.vue";
+
+export default {
+  name: "Home",
+  components: { PxAssetsTable },
+};
+</script>
